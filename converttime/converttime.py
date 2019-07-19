aee61942fa7417c18dcbf15acdf590261003e93a
Ian Paterson <ian.paterson@orionhealth.com>
2019-07-19 13:33:15 +1200

    Update converttime to display now time

--- converttime/converttime.py
+++ converttime/converttime.py
@@ -10,12 +10,12 @@ from datetime import timezone, datetime
 import argparse
 import sys
 
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def converttime(inputTime):
+def convertFloat(inputTime):
     try:
         floatTime = float(inputTime)
     except ValueError:
@@ -28,18 +28,31 @@ def converttime(inputTime):
     except ValueError:
         print("%f milliseconds since epoch is:" % floatTime)
         dt = datetime.fromtimestamp(floatTime/1000.0, timezone.utc)
+    return dt
+
+def printtime(dt, outputTimeZone):
     print("local time:")
     print(dt.astimezone().isoformat(" "))
     print("UTC time:")
     print(dt.astimezone(timezone.utc).isoformat(" "))
 
+def converttime(inputTime, inputTimeZone, outputTimeZone):
+    if inputTime == "now":
+      dt = convertFloat(time.time())
+    else:
+      dt = convertFloat(inputTime)
+
+    printtime(dt, outputTimeZone)
+
 def main():
     parser = argparse.ArgumentParser('Convert unix time to ISO-8601')
     parser.add_argument('time', help='Time in either ms or s since epoch')
+    parser.add_argument('-itz', help='Input timezone or offset')
+    parser.add_argument('-otz', help='Output timezone or offset')
     parser.add_argument('-v', '--version', action='version',
         version=VERSION, help='Print the version')
     args = parser.parse_args()
-    converttime(args.time)
+    converttime(args.time, args.itz, args.otz)
 
 if __name__ == "__main__":
     main()
